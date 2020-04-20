789f590112de55bbd15c2e352de4e561dfcef2ca
谢昆 <xiekun@sunlands.com>
2020-04-20 09:57:34 +0800

    SQLAlchemy的事务管理：try ... except 是简单的事务捕捉，嵌套事务需要在try里面添加with session.begin_nested()，事务的提交最后由外层的commit执行，with执行完毕，内层session自动托管到外层事务上 参考：https://blog.csdn.net/u012089823/article/details/94621398

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -31,28 +31,31 @@ class AdminService(object):
             print(resStr)
 
         # 创建新的admin对象
+        # try ... except 是简单的事务捕捉，嵌套事务需要在try里面添加with session.begin_nested()
+        # 事务的提交最后由外层的commit执行，with执行完毕，内层session自动托管到外层事务上
         try:
             admin = self.create_admin()
 
             print(type(admin))
             print("创建新用户", admin.name)
             # # 先插入数据，然后等所有逻辑操作做完之后，再db.session.commit()，事务才会起效
-            # a = 1 / 0
+            a = 1 / 0
 
-            # 提交事务
-            db.session.commit()
-
-        except BaseException:
-            print("创建异常，事务回滚")
+        except BaseException as e:
+            print("创建异常，事务回滚", e)
             db.session.rollback()
             return '创建异常，事务已经回滚'
+
+        # 提交事务
+        db.session.commit()
+
         return admin.name
 
     # 插入数据
     def create_admin(self):
         from werkzeug.security import generate_password_hash
         admin = Admin(
-            name="mtianyan14",
+            name="mtianyan15",
             pwd=generate_password_hash("123456"),
             is_super=0,
             role_id=2
